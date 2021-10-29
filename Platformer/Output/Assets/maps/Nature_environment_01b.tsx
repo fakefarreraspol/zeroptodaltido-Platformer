bfb9fb94fe673e6e9e8b856dc55fd1372b0d0f95
ª̵̢̳̯̹̗̘̮̈́̽̽̂̽̿̃̔̈́̔͠ª̵̢̳̯̹̗̘̮̈́̽̽̂̽̿̃̔̈́̔͠ª̵̢̳̯̹̗̘̮̈́̽̽̂̽̿̃̔̈́̔͠ <79444349+fakefarreraspol@users.noreply.github.com>
2021-10-29 11:32:13 +0200

    a

--- Platformer/Output/Assets/maps/Nature_environment_01b.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.5" name="Nature_environment_01b" tilewidth="48" tileheight="48" tilecount="133" columns="19">
- <image source="Nature_environment_01b.png" width="912" height="336"/>
-</tileset>
